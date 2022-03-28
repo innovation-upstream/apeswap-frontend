3bfe79d2f61e1d69f63248684d1c0e2f4bf65f9d
Justus Onuoha <valzey87@gmail.com>
2022-03-28 11:51:05 +0100

    chore: Create footer

---
+++ src/components/Footer/FooterLinks.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/** @jsxImportSource theme-ui */
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { Box, Flex } from 'theme-ui'
+import { Text, Svg } from '@innovationupstream/apeswap-uikit'
+import { footerLinks } from './Footer.data'
+import styles from './styles'
+
+const FooterLinks: React.FC = () => {
+  const [open, setOpen] = useState(new Array(footerLinks.length).fill(false))
+
+  const toggleOpen = (index: number) => {
+    const newOpen = [...open]
+    newOpen[index] = !newOpen[index]
+    setOpen(newOpen)
+  }
+
+  return (
+    <Flex sx={styles.linkSection}>
+      {footerLinks.map((col, index) => (
+        <Box>
+          <Flex sx={styles.linkTitle} onClick={() => toggleOpen(index)}>
+            <Text color="yellow">{col.title}</Text>
+            <span sx={{ display: ['block', 'none', 'none'] }}>
+              <Svg width="8px" icon="caret" direction="down" />
+            </span>
+          </Flex>
+          <ul
+            sx={{
+              ...styles.list,
+              display: [open[index] ? 'block' : 'none', 'block', 'block'],
+            }}
+          >
+            {col.links.map((link) => (
+              <li sx={{ marginTop: 7 }}>
+                <Link href={link.href} passHref>
+                  <a>
+                    <Text sx={{ fontSize: '16px' }}>{link.label}</Text>
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </Box>
+      ))}
+    </Flex>
+  )
+}
+
+export default FooterLinks
