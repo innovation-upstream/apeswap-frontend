d0854d173a00cc8efee4cdf3b46a2dd07b68302f
Justus Onuoha <valzey87@gmail.com>
2022-03-30 16:11:07 +0100

    chore: Fix build

--- src/pages/farms/history.tsx
+++ src/pages/farms/history.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { GetServerSideProps } from 'next'
+import BigNumber from 'bignumber.js'
 import { getServerSideGenericProps } from 'components/getServersideProps'
 import Farms from 'views/Farms'
 import DualFarms from 'views/DualFarms'
 import { setFarmsPublicData } from 'state/farms'
+import fetchFarmLpAprs from 'state/stats/getFarmLpAprs'
+import fetchLpPrices from 'state/lpPrices/fetchLpPrices'
 import fetchPrices from 'state/tokenPrices/fetchPrices'
 import fetchFarms from 'state/farms/fetchFarms'
 import fetchDualFarms from 'state/dualFarms/fetchDualFarms'
 import { CHAIN_ID } from 'config/constants/chains'
-import { wrapper } from '../../state'
+import { wrapper } from 'state'
 
 export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps((store) => async (context) => {
   const initialProps = await getServerSideGenericProps(context)
@@ -21,7 +24,13 @@ export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps
     if (chainId === CHAIN_ID.MATIC || chainId === CHAIN_ID.MATIC_TESTNET) {
       farmData = await fetchDualFarms(tokenPrices, chainId)
     } else {
-      farmData = await fetchFarms(chainId)
+      const lpTokenPrices = await fetchLpPrices(chainId)
+      const farmLpAprs = await fetchFarmLpAprs(chainId)
+      const bananaPrice = (
+        new BigNumber(tokenPrices?.find((token) => token.symbol === 'BANANA')?.price) || new BigNumber(0)
+      ).toString()
+
+      farmData = await fetchFarms(chainId, lpTokenPrices, new BigNumber(bananaPrice), farmLpAprs)
     }
   } catch (e) {
     console.warn(e)
--- src/pages/farms/index.tsx
+++ src/pages/farms/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { GetServerSideProps } from 'next'
+import BigNumber from 'bignumber.js'
 import { getServerSideGenericProps } from 'components/getServersideProps'
 import Farms from 'views/Farms'
 import DualFarms from 'views/DualFarms'
 import { ViewMode } from 'views/DualFarms/components/types'
 import { setFarmsPublicData } from 'state/farms'
 import { setDualFarmsPublicData } from 'state/dualFarms'
+import fetchFarmLpAprs from 'state/stats/getFarmLpAprs'
+import fetchLpPrices from 'state/lpPrices/fetchLpPrices'
 import fetchPrices from 'state/tokenPrices/fetchPrices'
 import fetchFarms from 'state/farms/fetchFarms'
 import fetchDualFarms from 'state/dualFarms/fetchDualFarms'
 import { CHAIN_ID } from 'config/constants/chains'
-import { wrapper } from '../../state'
+import { wrapper } from 'state'
 
 export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps((store) => async (context) => {
   const initialProps = await getServerSideGenericProps(context)
@@ -24,7 +27,13 @@ export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps
     if (chainId === CHAIN_ID.MATIC || chainId === CHAIN_ID.MATIC_TESTNET) {
       dualFarmsData = await fetchDualFarms(tokenPrices, chainId)
     } else {
-      farmData = await fetchFarms(chainId)
+      const lpTokenPrices = await fetchLpPrices(chainId)
+      const farmLpAprs = await fetchFarmLpAprs(chainId)
+      const bananaPrice = (
+        new BigNumber(tokenPrices?.find((token) => token.symbol === 'BANANA')?.price) || new BigNumber(0)
+      ).toString()
+
+      farmData = await fetchFarms(chainId, lpTokenPrices, new BigNumber(bananaPrice), farmLpAprs)
     }
   } catch (e) {
     console.warn(e)
