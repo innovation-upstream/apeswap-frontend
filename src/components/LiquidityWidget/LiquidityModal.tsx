f8dd886fb859543414d3bd46b7c557e51aa400f0
Justus Onuoha <valzey87@gmail.com>
2022-05-17 13:00:08 +0100

    feat: Interactive roi calculator

--- src/components/LiquidityWidget/LiquidityModal.tsx
+++ src/components/LiquidityWidget/LiquidityModal.tsx
@@ -5,23 +5,33 @@ import { HelpIcon, ModalProvider } from '@apeswapfinance/uikit'
 import { Flex } from 'theme-ui'
 import { merge } from 'lodash'
 import { useTranslation } from 'contexts/Localization'
+import useIsMobile from 'hooks/useIsMobile'
+import { useMintActionHandlers } from 'state/mint/hooks'
 
 interface LiquidityModalProps {
   widgetProps?: Record<string, any>
   modalProps?: Record<string, any>
-  handleClose?: () => void
+  onDismiss?: () => void
 }
 
-const LiquidityModal: React.FC<LiquidityModalProps> = ({ widgetProps, handleClose, modalProps: newModalProps }) => {
+const LiquidityModal: React.FC<LiquidityModalProps> = ({ widgetProps, onDismiss, modalProps: newModalProps }) => {
   const { t } = useTranslation()
+  const isMobile = useIsMobile()
+  const { onFieldAInput } = useMintActionHandlers(false)
+
   const modalProps = {
-    minWidth: '385px',
-    maxWidth: '385px',
+    minWidth: isMobile ? '320px' : '385px',
+    maxWidth: isMobile ? '90vw' : '385px',
     style: {
       zIndex: 20,
       overflowY: 'auto',
       maxHeight: 'calc(100% - 30px)',
     },
+  }
+
+  const handleClose = () => {
+    onFieldAInput('')
+    onDismiss()
   }
 
   return (
