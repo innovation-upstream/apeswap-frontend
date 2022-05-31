7ed65d963a51872209940de02c68745b7d61a6d3
Gurdeep <gurdeep.singh@innovationupstream.com>
2022-05-31 22:29:15 +0530

    feat: Errorresolved

--- src/components/LiquidityWidget/ConnectButton.tsx
+++ src/components/LiquidityWidget/ConnectButton.tsx
@@ -11,7 +11,7 @@ const ConnectButton = () => {
   const { onPresentConnectModal } = useWalletModal(login, logout, t)
 
   return (
-    <Button onClick={onPresentConnectModal} csx={styles.button}>
+    <Button onClick={onPresentConnectModal} sx={styles.button}>
       {t('CONNECT WALLET')}
     </Button>
   )
