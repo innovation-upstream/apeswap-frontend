05d804adc7551f354d65cd6324516b0dec52da2b
Ape Fede <apefede09@gmail.com>
2022-07-06 11:37:55 -0300

    fix: Add scrollbar for small screen sizes

--- src/views/Topup/MoonpayModal.tsx
+++ src/views/Topup/MoonpayModal.tsx
@@ -1,10 +1,20 @@
+/** @jsxImportSource theme-ui */
 import React from 'react'
-import { Modal, ModalProps } from '@apeswapfinance/uikit'
+import { Modal, ModalProps } from '@ape.swap/uikit'
 import MoonPayIframe from './MoonFrame'
 
 export default function MoonPayModal({ onDismiss }: ModalProps) {
+  const modalProps = {
+    minWidth: '385px',
+    maxWidth: '385px',
+    style: {
+      zIndex: 10,
+      overflowY: 'auto',
+      maxHeight: 'calc(100% - 30px)',
+    },
+  }
   return (
-    <Modal title="Top up with Moonpay" minWidth="400px" onDismiss={onDismiss}>
+    <Modal title="Top up with Moonpay" minWidth="400px" onDismiss={onDismiss} {...modalProps}>
       <MoonPayIframe />
     </Modal>
   )
