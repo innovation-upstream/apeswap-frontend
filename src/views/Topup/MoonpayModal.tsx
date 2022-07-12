0d5118714235da6a082a044f32ef1809d5add3d5
Ape Fede <apefede09@gmail.com>
2022-07-11 21:23:46 -0300

    chore: Size tweaks

--- src/views/Topup/MoonpayModal.tsx
+++ src/views/Topup/MoonpayModal.tsx
@@ -1,22 +1,25 @@
 /** @jsxImportSource theme-ui */
 import React from 'react'
-import useIsMobile from 'hooks/useIsMobile'
 import { Modal, ModalProps } from '@ape.swap/uikit'
 import MoonPayIframe from './MoonFrame'
 
 export default function MoonPayModal({ onDismiss }: ModalProps) {
-  const isMobile = useIsMobile()
   const modalProps = {
-    minWidth: isMobile ? '90%' : '437px',
-    maxWidth: '437px',
     style: {
       zIndex: 10,
       overflowY: 'auto',
       maxHeight: 'calc(100% - 30px)',
     },
+    sx: {
+      minWidth: '437px',
+      '@media screen and (max-width: 437px)': {
+        minWidth: '95%',
+      },
+      maxWidth: '437px',
+    },
   }
   return (
-    <Modal title="Top up with Moonpay" minWidth="400px" onDismiss={onDismiss} {...modalProps}>
+    <Modal title="Buy crypto with MoonPay" onDismiss={onDismiss} {...modalProps}>
       <MoonPayIframe />
     </Modal>
   )
