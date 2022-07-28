1ee13618e0ea6f791911a0ca90e1a340c92ab118
Harambe-Nakamoto <haramben@protonmail.com>
2022-07-27 20:03:46 -0500

    chore: Update blocks

--- src/hooks/useCurrentBlockTimestamp.ts
+++ src/hooks/useCurrentBlockTimestamp.ts
@@ -1,9 +1,14 @@
+import { Multicall } from 'config/abi/types'
+import contracts from 'config/constants/contracts'
+import multi from 'config/abi/Multicall.json'
 import { BigNumber } from 'ethers'
 import { useSingleCallResult } from '../state/multicall/hooks'
-import { useMulticallContract } from './useContract'
+import useActiveWeb3React from './useActiveWeb3React'
+import useContract from './useContract'
 
 // gets the current timestamp from the blockchain
 export default function useCurrentBlockTimestamp(): BigNumber | undefined {
-  const multicall = useMulticallContract()
+  const { chainId } = useActiveWeb3React()
+  const multicall = useContract(multi, contracts.mulltiCall[chainId], false) as Multicall
   return useSingleCallResult(multicall, 'getCurrentBlockTimestamp')?.result?.[0]
 }
