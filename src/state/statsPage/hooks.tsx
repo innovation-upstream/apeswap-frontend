5f035dadbe1383e92b033bbd9f96d88972943f89
Naked Nuts <nakednuts@protonmail.com>
2022-08-18 16:45:35 -0300

    refactor: Stats data fetch hook

--- src/state/statsPage/hooks.tsx
+++ src/state/statsPage/hooks.tsx
@@ -1,10 +1,10 @@
 import React, { createContext, ReactNode, useContext, useEffect, useState } from 'react'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
-import { statsApi } from './api'
 
 import { ApiResponse, ChainOption } from './types'
 import { mapChain, rawToPortfolio, PortfolioData, rawToProjected, ProjectedData } from './mappings'
 import { NftInfo, rawToNfts } from './mappings/rawToNfts'
+import { fetchStatsData } from './api'
 
 interface StatsProviderProps {
   children: ReactNode
@@ -37,13 +37,14 @@ export function StatsProvider({ children }: StatsProviderProps) {
 
       const chain = mapChain(selectedChain)
 
-      statsApi.get(`/${account}${chain ? `?chain=${chain}` : ''}`).then((response) => {
-        setStats(response.data)
-        setPortfolioData(rawToPortfolio(response.data))
-        setProjectedData(rawToProjected(response.data))
-        setNfts(rawToNfts(response.data))
-        setLoading(false)
-      })
+      const statsData = await fetchStatsData(`${account}${chain ? `?chain=${chain}` : ''}`)
+
+      setStats(statsData)
+      setPortfolioData(rawToPortfolio(statsData))
+      setProjectedData(rawToProjected(statsData))
+      setNfts(rawToNfts(statsData))
+
+      setLoading(false)
     }
 
     if (account) {
