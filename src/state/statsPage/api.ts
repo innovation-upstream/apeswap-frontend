0acb9c523632872d7f6f4e0ce7f4f7244addced4
Naked Nuts <nakednuts@protonmail.com>
2022-08-18 16:43:31 -0300

    feat: Added `axios-retry` and initial state for stats data

--- src/state/statsPage/api.ts
+++ src/state/statsPage/api.ts
@@ -1,10 +1,34 @@
 import axios from 'axios'
+import axiosRetry from 'axios-retry'
+import { initialStatsData } from './mappings'
 
 const baseUrl = {
   local: 'http://localhost:3333/stats',
   prod: 'https://jellyfish-app-vtrfw.ondigitalocean.app/stats',
 }
 
-export const statsApi = axios.create({
-  baseURL: baseUrl.prod,
-})
+let tries = 0
+let cacheStats = []
+
+export const fetchStatsData = async (params: string) => {
+  try {
+    if (tries === 0) {
+      axiosRetry(axios, {
+        retries: 5,
+        retryCondition: () => true,
+      })
+      tries++
+      const response = await axios.get(`${baseUrl.local}/${params}`)
+      const statsResponse = await response.data
+      if (statsResponse.statusCode === 500) {
+        return initialStatsData
+      }
+      cacheStats = statsResponse
+      return statsResponse
+    }
+    return cacheStats
+  } catch (error) {
+    tries = 0
+    return initialStatsData
+  }
+}
