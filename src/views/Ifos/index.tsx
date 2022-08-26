d4d5687d5f11b0e4f091393ea89d4d0e52f67482
Obie Dobo <obiedobo@gmail.com>
2022-08-25 21:14:57 -0500

    fix: Iao price

--- src/views/Ifos/index.tsx
+++ src/views/Ifos/index.tsx
@@ -9,6 +9,7 @@ import Ideology from './components/Ideology/Ideology'
 import IfoPastProjectSwiper from './components/PastProjectSwiper/IfoPastProjectSwiper'
 import { TabOption } from './types'
 import IfoProjectCard from './components/IfoCard/ProjectCard/IfoProjectCard'
+import { useFetchTokenPrices } from 'state/tokenPrices/hooks'
 
 const Ifos = () => {
   const { t } = useTranslation()
@@ -16,6 +17,7 @@ const Ifos = () => {
   const activeIfoId = ifos(t).find((ifo) => ifo.isActive).id
   const [tabOption, setTabOption] = React.useState<TabOption>('current')
   const [projectId, setProjectId] = React.useState<string | undefined>(activeIfoId)
+  useFetchTokenPrices()
 
   const handleTabSelectionChange = (option: TabOption) => {
     setTabOption(option)
