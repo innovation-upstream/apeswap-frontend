89964e6246dad64016c6311fee8bf87da3696af1
Harambe-Nakamoto <haramben@protonmail.com>
2022-09-29 13:29:46 -0500

    chore: Switch to telos config

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -14,10 +14,8 @@ const EXCHANGE_SUBGRAPH_URL = 'https://graph.apeswap.finance/subgraphs/name/ape-
 const EXCHANGE_POLYGON_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/apeswapfinance/dex-polygon'
 
 // TODO: Switch this back to main
-export const apeswapListUrl =
-  process.env.REACT_APP_LISTS_CONFIG_URL ||
-  'https://raw.githubusercontent.com/ApeSwapFinance/apeswap-lists/feat/tlos/config'
-//'https://raw.githubusercontent.com/ApeSwapFinance/apeswap-lists/main/config'
+export const apeswapListUrl = 'https://raw.githubusercontent.com/ApeSwapFinance/apeswap-lists/feat/tlos/config'
+//  process.env.REACT_APP_LISTS_CONFIG_URL || 'https://raw.githubusercontent.com/ApeSwapFinance/apeswap-lists/main/config'
 /* eslint-disable camelcase */
 
 export interface TradePair {
