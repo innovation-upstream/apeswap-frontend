fc7f2d8e0a2552ff79c48d8cc050555c91fe7cd3
Harambe-Nakamoto <harambeN@protonmail.com>
2022-10-15 17:37:06 -0700

    chore: Fix inactive

--- src/views/JungleFarms/JungleFarms.tsx
+++ src/views/JungleFarms/JungleFarms.tsx
@@ -23,6 +23,8 @@ import ListViewMenu from '../../components/ListViewMenu'
 import HarvestAll from './components/Actions/HarvestAll'
 import useActiveWeb3React from '../../hooks/useActiveWeb3React'
 import { useSetZapOutputList } from 'state/zap/hooks'
+import { ChainId } from '@ape.swap/sdk'
+import useCurrentTime from 'hooks/useTimer'
 
 const NUMBER_OF_FARMS_VISIBLE = 10
 
@@ -47,6 +49,7 @@ const JungleFarms: React.FC = () => {
   const loadMoreRef = useRef<HTMLDivElement>(null)
   const { jungleFarmTags } = useJungleFarmTags(chainId)
   const { jungleFarmOrderings } = useJungleFarmOrderings(chainId)
+  const currentTime = useCurrentTime()
   const handleChangeQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value)
   }
@@ -70,7 +73,11 @@ const JungleFarms: React.FC = () => {
   }, [observerIsSet])
 
   const currJungleFarms = allJungleFarms.map((farm) => {
-    return { ...farm, isFinished: farm.isFinished || currentBlock > farm.endBlock }
+    return {
+      ...farm,
+      isFinished:
+        farm.isFinished || farm.rewardsPerSecond ? currentTime / 1000 > farm.endBlock : currentBlock > farm.endBlock,
+    }
   })
 
   const farmsWithHarvestAvailable = currJungleFarms.filter((farm) =>
@@ -190,7 +197,7 @@ const JungleFarms: React.FC = () => {
         <ListViewLayout>
           <Banner
             banner="jungle-farms"
-            title={chainId === 40 ? t('Telos Farms') : t('Jungle Farms')}
+            title={chainId === ChainId.TLOS ? t('Telos Farms') : t('Jungle Farms')}
             link="https://apeswap.gitbook.io/apeswap-finance/product-and-features/stake/farms"
             listViewBreak
             maxWidth={1130}
