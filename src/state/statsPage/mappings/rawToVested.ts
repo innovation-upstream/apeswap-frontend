ee2edf4867ab3ce8dd4fea20edadbe92f7470a33
Naked Nuts <nakednuts@protonmail.com>
2022-12-08 00:28:24 -0300

    refactor: Vested earnings map

--- src/state/statsPage/mappings/rawToVested.ts
+++ src/state/statsPage/mappings/rawToVested.ts
@@ -5,12 +5,9 @@ export type Vested = (Bill | Iao) & { chain: Chain }
 export function rawToVested({ userStats }: ApiResponse) {
   const vestedEarnings: Vested[] = []
 
-  userStats.forEach((chainInfo) => {
-    if (chainInfo.bills?.length)
-      chainInfo.bills?.forEach((bill) => vestedEarnings.push({ ...bill, chain: chainInfo.chainId }))
-
-    if (chainInfo.iaos?.length)
-      chainInfo.iaos?.forEach((iao) => vestedEarnings.push({ ...iao, chain: chainInfo.chainId }))
+  userStats.forEach(({ bills, iaos, chainId }) => {
+    bills?.forEach((bill) => vestedEarnings.push({ ...bill, chain: chainId }))
+    iaos?.forEach((iao) => vestedEarnings.push({ ...iao, chain: chainId }))
   })
 
   return vestedEarnings.sort((a, b) => (a.vestingTimeRemaining > b.vestingTimeRemaining ? -1 : 1))
