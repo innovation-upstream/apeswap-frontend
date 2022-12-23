4fa2e02e5d09346aaf290337d78ce24debf7c39b
Obie Dobo <obiedobo@gmail.com>
2022-12-23 12:09:22 -0500

    chore: Update prod stats URL

--- src/state/statsPage/api.ts
+++ src/state/statsPage/api.ts
@@ -2,14 +2,12 @@ import axios from 'axios'
 import axiosRetry from 'axios-retry'
 import { initialStatsData } from './mappings'
 
-// TODO: remove test URL before launching Telos chain.
 const baseUrl = {
   local: 'http://localhost:3333/stats',
   prod: 'https://apeswap.api.pacoca.io/stats',
-  test: 'https://whale-app-mkbgq.ondigitalocean.app/stats',
 }
 
-const statsApi = axios.create({ baseURL: baseUrl.test })
+const statsApi = axios.create({ baseURL: baseUrl.prod })
 
 axiosRetry(statsApi, {
   retries: 5,
