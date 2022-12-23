22985e84bd99ec83f5d524e2060464d9f59c21d6
Ape Fede <75544302+ape-fede@users.noreply.github.com>
2022-12-23 12:44:57 -0300

    chore: Fix docs link lag

--- src/components/FloatingDocs/index.tsx
+++ src/components/FloatingDocs/index.tsx
@@ -1,15 +1,26 @@
 /** @jsxImportSource theme-ui */
 import React from 'react'
-import { Flex, Svg, useMatchBreakpoints } from '@ape.swap/uikit'
-import { useRouteLinks } from 'hooks/useRouteLinks'
+import { Flex, Svg } from '@ape.swap/uikit'
 import { FloatingDocsProps } from './types'
+import { useHistory } from 'react-router-dom'
+import useActiveWeb3React from '../../hooks/useActiveWeb3React'
+import { NETWORK_LABEL } from '../../config/constants/chains'
+import { DOC_LINKS, Farms, routeNames } from '../../config/constants'
+import useIsMobile from '../../hooks/useIsMobile'
 
 export const FloatingDocs: React.FC<FloatingDocsProps> = ({ iconProps, ...props }) => {
-  const { isLg, isXl, isXxl } = useMatchBreakpoints()
-  const isMobile = !isLg && !isXl && !isXxl
-  const { link } = useRouteLinks()
+  const isMobile = useIsMobile()
+  const history = useHistory()
+  const { chainId } = useActiveWeb3React()
+  const getDocsLink = () => {
+    const { pathname } = history.location
+    const networkLabel = NETWORK_LABEL[chainId]
+    const farmTypes = Farms[networkLabel]
+    DOC_LINKS['FARMS'] = `https://apeswap.gitbook.io/apeswap-finance/product-and-features/stake/farms/${farmTypes}`
+    return DOC_LINKS[routeNames[pathname]]
+  }
 
-  return link ? (
+  return (
     <Flex
       sx={{
         position: 'fixed',
@@ -18,12 +29,12 @@ export const FloatingDocs: React.FC<FloatingDocsProps> = ({ iconProps, ...props 
         zIndex: 5,
         cursor: 'pointer',
       }}
-      onClick={() => window.open(link, '_blank')}
+      onClick={() => window.open(getDocsLink(), '_blank')}
       {...props}
     >
       <Svg icon="docs" width={isMobile ? 40 : 50} color="primaryBright" {...iconProps} />
     </Flex>
-  ) : null
+  )
 }
 
 export default FloatingDocs
