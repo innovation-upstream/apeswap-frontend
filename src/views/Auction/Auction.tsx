65f293ca2f1c07685df42ff53257478a9b6971f4
Ape Fede <75544302+ape-fede@users.noreply.github.com>
2022-12-23 16:42:39 -0300

    chore: Fix banner position

--- src/views/Auction/Auction.tsx
+++ src/views/Auction/Auction.tsx
@@ -6,10 +6,10 @@ import Banner from 'components/Banner'
 import { useAuctions, useFetchAuctions } from 'state/hooks'
 import { useTranslation } from 'contexts/Localization'
 import Positions from './components/Positions'
-import Container from './components/Container'
 import History from './components/History'
 import ListYourNfa from './components/Actions/ListYourNfa'
 import { AuctionCardsWrapper, MoreInfoWrapper, PageWrapper, SplitWrapper } from './styles'
+import ListViewLayout from '../../components/layout/ListViewLayout'
 
 const Auction: React.FC = () => {
   useFetchAuctions()
@@ -19,44 +19,55 @@ const Auction: React.FC = () => {
   const isDesktop = isXxl || isXl
   return (
     <SwiperProvider>
-      <Container>
-        <Flex alignItems="center" justifyContent="center">
-          <Flex alignSelf="center" style={{ width: '1200px' }}>
-            <Banner
-              banner="auction"
-              link="https://apeswap.gitbook.io/apeswap-finance/product-and-features/collect/nfa-auction-house"
-              title={t('Nfa Auction')}
-              margin="30px 10px 20px 10px"
-            />
-          </Flex>
-        </Flex>
-        <PageWrapper>
-          <Flex sx={{ width: '100%', justifyContent: 'center' }}>
-            <MoreInfoWrapper>
-              <Button size="sm" style={{ marginRight: '10px', height: '36px' }}>
-                <a
-                  href="https://apeswap.gitbook.io/apeswap-finance/product-information/non-fungible-apes-nfas/nfa-auction-house"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {t('HOW IT WORKS')}
-                </a>
-              </Button>
-              <ListYourNfa />
-            </MoreInfoWrapper>
-          </Flex>
-          {auctions ? (
-            <SplitWrapper>
-              <AuctionCardsWrapper>{auctions && <Positions auctions={auctions} />}</AuctionCardsWrapper>
-            </SplitWrapper>
-          ) : (
-            <Flex sx={{ justifyContent: 'center' }}>
-              <Spinner size={200} />
+      <Flex
+        sx={{
+          position: 'relative',
+          top: '30px',
+          width: '100%',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItem: 'center',
+          marginBottom: '80px',
+        }}
+      >
+        <ListViewLayout>
+          <Flex alignItems="center" justifyContent="center">
+            <Flex alignSelf="center" style={{ width: '1200px' }}>
+              <Banner
+                banner="auction"
+                link="https://apeswap.gitbook.io/apeswap-finance/product-and-features/collect/nfa-auction-house"
+                title={t('Nfa Auction')}
+              />
             </Flex>
-          )}
-          {isDesktop && <History />}
-        </PageWrapper>
-      </Container>
+          </Flex>
+          <PageWrapper>
+            <Flex sx={{ width: '100%', justifyContent: 'center', marginTop: '20px' }}>
+              <MoreInfoWrapper>
+                <Button size="sm" style={{ marginRight: '10px', height: '36px' }}>
+                  <a
+                    href="https://apeswap.gitbook.io/apeswap-finance/product-information/non-fungible-apes-nfas/nfa-auction-house"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {t('HOW IT WORKS')}
+                  </a>
+                </Button>
+                <ListYourNfa />
+              </MoreInfoWrapper>
+            </Flex>
+            {auctions ? (
+              <SplitWrapper>
+                <AuctionCardsWrapper>{auctions && <Positions auctions={auctions} />}</AuctionCardsWrapper>
+              </SplitWrapper>
+            ) : (
+              <Flex sx={{ justifyContent: 'center' }}>
+                <Spinner size={200} />
+              </Flex>
+            )}
+            {isDesktop && <History />}
+          </PageWrapper>
+        </ListViewLayout>
+      </Flex>
     </SwiperProvider>
   )
 }
