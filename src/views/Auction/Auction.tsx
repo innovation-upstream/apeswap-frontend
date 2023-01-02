3976edb5c4a6370b4f2e94b72045715cb30c6a3d
Ape Fede <75544302+ape-fede@users.noreply.github.com>
2023-01-02 10:39:21 -0300

    chore: Minor styles adjustments

--- src/views/Auction/Auction.tsx
+++ src/views/Auction/Auction.tsx
@@ -8,7 +8,7 @@ import { useTranslation } from 'contexts/Localization'
 import Positions from './components/Positions'
 import History from './components/History'
 import ListYourNfa from './components/Actions/ListYourNfa'
-import { AuctionCardsWrapper, MoreInfoWrapper, PageWrapper, SplitWrapper } from './styles'
+import { MoreInfoWrapper, styles } from './styles'
 import ListViewLayout from '../../components/layout/ListViewLayout'
 
 const Auction: React.FC = () => {
@@ -17,53 +17,36 @@ const Auction: React.FC = () => {
   const { t } = useTranslation()
   return (
     <SwiperProvider>
-      <Flex
-        sx={{
-          position: 'relative',
-          top: '30px',
-          width: '100%',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItem: 'center',
-          marginBottom: '30px',
-        }}
-      >
+      <Flex sx={styles.mainContainer}>
         <ListViewLayout>
-          <Flex alignItems="center" justifyContent="center">
-            <Flex alignSelf="center" style={{ width: '1200px' }}>
-              <Banner
-                banner="auction"
-                link="https://apeswap.gitbook.io/apeswap-finance/product-and-features/collect/nfa-auction-house"
-                title={t('Nfa Auction')}
-              />
-            </Flex>
+          <Banner
+            banner="auction"
+            link="https://apeswap.gitbook.io/apeswap-finance/product-and-features/collect/nfa-auction-house"
+            title={t('Nfa Auction')}
+            maxWidth={1130}
+          />
+          <Flex sx={{ width: '100%', justifyContent: 'center', marginTop: '20px' }}>
+            <MoreInfoWrapper>
+              <Button size="sm" style={{ marginRight: '10px', height: '36px' }}>
+                <a
+                  href="https://apeswap.gitbook.io/apeswap-finance/product-information/non-fungible-apes-nfas/nfa-auction-house"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {t('HOW IT WORKS')}
+                </a>
+              </Button>
+              <ListYourNfa />
+            </MoreInfoWrapper>
           </Flex>
-          <PageWrapper>
-            <Flex sx={{ width: '100%', justifyContent: 'center', marginTop: '20px' }}>
-              <MoreInfoWrapper>
-                <Button size="sm" style={{ marginRight: '10px', height: '36px' }}>
-                  <a
-                    href="https://apeswap.gitbook.io/apeswap-finance/product-information/non-fungible-apes-nfas/nfa-auction-house"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {t('HOW IT WORKS')}
-                  </a>
-                </Button>
-                <ListYourNfa />
-              </MoreInfoWrapper>
+          {auctions ? (
+            <Positions auctions={auctions} />
+          ) : (
+            <Flex sx={{ justifyContent: 'center' }}>
+              <Spinner size={200} />
             </Flex>
-            {auctions ? (
-              <SplitWrapper>
-                <AuctionCardsWrapper>{auctions && <Positions auctions={auctions} />}</AuctionCardsWrapper>
-              </SplitWrapper>
-            ) : (
-              <Flex sx={{ justifyContent: 'center' }}>
-                <Spinner size={200} />
-              </Flex>
-            )}
-            <History />
-          </PageWrapper>
+          )}
+          <History />
         </ListViewLayout>
       </Flex>
     </SwiperProvider>
--- src/views/Auction/styles.ts
+++ src/views/Auction/styles.ts
@@ -1,29 +1,5 @@
 import styled from 'styled-components'
 
-export const PageWrapper = styled.div`
-  display: flex;
-  margin-bottom: 30px;
-  flex-direction: column;
-`
-
-export const AuctionCardsWrapper = styled.div`
-  display: flex;
-  max-width: 100%;
-  overflow-y: auto;
-  overflow-x: hidden;
-  & > div {
-    flex: 1;
-    overflow: hidden;
-  }
-`
-export const SplitWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr 12px 0;
-  flex: 1;
-  overflow: hidden;
-`
-
 export const MoreInfoWrapper = styled.div`
   display: flex;
   width: 100%;
@@ -33,3 +9,15 @@ export const MoreInfoWrapper = styled.div`
     justify-content: flex-start;
   }
 `
+
+export const styles = {
+  mainContainer: {
+    position: 'relative',
+    top: '30px',
+    width: '100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItem: 'center',
+    marginBottom: '30px',
+  },
+}
