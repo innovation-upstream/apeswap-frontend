42c44d4dc117ad44c96f0d91e876273a864f55cd
Harambe-Nakamoto <haramben@protonmail.com>
2023-03-30 16:19:08 -0500

    chore: Change switch condition

--- src/views/Dex/components/DexNav/index.tsx
+++ src/views/Dex/components/DexNav/index.tsx
@@ -33,6 +33,7 @@ const DexNav: React.FC<DexNavProps> = ({ zapSettings }) => {
   const [onPresentSettingsModal] = useModal(<SettingsModal zapSettings={zapSettings} />)
 
   const v3Link = `${process.env.REACT_APP_APESWAP_URL}${pathname}`
+  const showV3Liquidity = onLiquidity && [ChainId.BSC, ChainId.MATIC].includes(chainId)
 
   return (
     <Flex sx={styles.dexNavContainer}>
@@ -80,51 +81,100 @@ const DexNav: React.FC<DexNavProps> = ({ zapSettings }) => {
         </Text>
       </Flex>
       <Flex sx={{ ...styles.navIconContainer }}>
-        {!onOrders || (onLiquidity && [ChainId.BSC, ChainId.MATIC].includes(chainId)) || chainId !== ChainId.TLOS ? (
-          <Flex
-            as={ThemeLink}
-            href={v3Link}
-            rel="noopener noreferrer"
-            // onClick={() => push(pathname.includes('/v2') ? '/add-liquidity' : '/add-liquidity/v2')}
-            sx={{
-              position: 'relative',
-              mr: ['0px', '0px', '10px'],
-              height: 'fit-content',
-              minWidth: 'fit-content',
-              alignItems: 'center',
-              cursor: 'pointer',
-              zIndex: 2,
-            }}
-          >
-            <Text
-              size="13px"
-              weight={700}
-              color="primaryBright"
-              sx={{ position: 'absolute', zIndex: 1, right: 3, mt: '2px' }}
-            >
-              V2
-            </Text>
-            <Switch
-              onClick={() => window.location.replace(v3Link)}
-              // onChange={() => push(pathname.includes('/v2') ? '/add-liquidity' : '/add-liquidity/v2')}
-              checked={false}
+        {!onOrders && chainId !== ChainId.TLOS ? (
+          onLiquidity ? (
+            showV3Liquidity && (
+              <Flex
+                as={ThemeLink}
+                href={v3Link}
+                rel="noopener noreferrer"
+                // onClick={() => push(pathname.includes('/v2') ? '/add-liquidity' : '/add-liquidity/v2')}
+                sx={{
+                  position: 'relative',
+                  mr: ['0px', '0px', '10px'],
+                  height: 'fit-content',
+                  minWidth: 'fit-content',
+                  alignItems: 'center',
+                  cursor: 'pointer',
+                  zIndex: 2,
+                }}
+              >
+                <Text
+                  size="13px"
+                  weight={700}
+                  color="primaryBright"
+                  sx={{ position: 'absolute', zIndex: 1, right: 3, mt: '2px' }}
+                >
+                  V2
+                </Text>
+                <Switch
+                  onClick={() => window.location.replace(v3Link)}
+                  // onChange={() => push(pathname.includes('/v2') ? '/add-liquidity' : '/add-liquidity/v2')}
+                  checked={false}
+                  sx={{
+                    mr: '0px',
+                    width: '50px',
+                    borderRadius: '10px',
+                    backgroundColor: 'yellow',
+                    '& > div': {
+                      transform: 'translateX(0%)',
+                    },
+                    'input:checked ~ &': {
+                      background: 'linear-gradient(90deg, rgba(161, 101, 82, 1) 0%, rgba(255, 179, 0, 1)) 100%',
+                      '> div': {
+                        transform: 'translateX(28px)',
+                      },
+                    },
+                  }}
+                />
+              </Flex>
+            )
+          ) : (
+            <Flex
+              as={ThemeLink}
+              href={v3Link}
+              rel="noopener noreferrer"
+              // onClick={() => push(pathname.includes('/v2') ? '/add-liquidity' : '/add-liquidity/v2')}
               sx={{
-                mr: '0px',
-                width: '50px',
-                borderRadius: '10px',
-                backgroundColor: 'yellow',
-                '& > div': {
-                  transform: 'translateX(0%)',
-                },
-                'input:checked ~ &': {
-                  background: 'linear-gradient(90deg, rgba(161, 101, 82, 1) 0%, rgba(255, 179, 0, 1)) 100%',
-                  '> div': {
-                    transform: 'translateX(28px)',
-                  },
-                },
+                position: 'relative',
+                mr: ['0px', '0px', '10px'],
+                height: 'fit-content',
+                minWidth: 'fit-content',
+                alignItems: 'center',
+                cursor: 'pointer',
+                zIndex: 2,
               }}
-            />
-          </Flex>
+            >
+              <Text
+                size="13px"
+                weight={700}
+                color="primaryBright"
+                sx={{ position: 'absolute', zIndex: 1, right: 3, mt: '2px' }}
+              >
+                V2
+              </Text>
+              <Switch
+                onClick={() => window.location.replace(v3Link)}
+                // onChange={() => push(pathname.includes('/v2') ? '/add-liquidity' : '/add-liquidity/v2')}
+                checked={false}
+                sx={{
+                  mr: '0px',
+                  width: '50px',
+                  borderRadius: '10px',
+                  backgroundColor: 'yellow',
+                  '& > div': {
+                    transform: 'translateX(0%)',
+                  },
+                  'input:checked ~ &': {
+                    background: 'linear-gradient(90deg, rgba(161, 101, 82, 1) 0%, rgba(255, 179, 0, 1)) 100%',
+                    '> div': {
+                      transform: 'translateX(28px)',
+                    },
+                  },
+                }}
+              />
+            </Flex>
+          )
         ) : (
           <></>
         )}
