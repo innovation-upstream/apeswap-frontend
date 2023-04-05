a394311db0e8c4298d41caeb1e6eaab4b7bbb704
Obie Dobo <obiedobo@gmail.com>
2023-04-05 17:39:48 -0400

    chore: Update homepage to handle V3 component

--- src/views/Homepage/components/WelcomeContent/slides/ApeSwapV3.tsx
+++ src/views/Homepage/components/WelcomeContent/slides/ApeSwapV3.tsx
@@ -3,38 +3,32 @@ import React from 'react'
 import { styles } from './styles'
 import { Button, Flex, Text } from '@ape.swap/uikit'
 import { Link, useHistory } from 'react-router-dom'
-import { ChainId } from '@ape.swap/sdk'
 import Bnb from './grayChains/bnb'
 import Poly from './grayChains/poly'
-import Tlos from './grayChains/Tlos'
-import Arbitrum from './grayChains/Arbitrum'
 import { Box, Image } from 'theme-ui'
 import { useTranslation } from 'contexts/Localization'
-import useSelectNetwork from 'hooks/useSelectNetwork'
 
 const ApeSwapV3 = () => {
   const { t } = useTranslation()
-  const { switchNetwork } = useSelectNetwork()
   const history = useHistory()
-
-  const handleNetworkSwitch = (chainId: ChainId) => {
-    history.push('/add-liquidity')
-    switchNetwork(chainId)
-  }
 
   return (
     <Flex sx={styles.slideContainer}>
       <Flex sx={styles.slideContent}>
-        <Text sx={{ ...styles.slideTitle }}>{t('Apeswap Liquidity V3')}</Text>
+        <Text sx={{ ...styles.slideTitle }}>{t('Do More With Your Liquidity')}</Text>
         <Text sx={styles.slideSubtitle}>
-          {t('Insert text about Liquidity V3 here, please dont make it super long.')}
+          {t('Leverage ApeSwap’s new V3 capabilities to concentrate your liquidity depth.')}
         </Text>
         <Flex sx={{ alignItems: 'center', marginTop: ['25px', '25px', '0px'] }}>
           <Text sx={styles.availableOn}>{t('AVAILABLE ON')}</Text>
-          <Bnb sx={{ marginRight: '10px', cursor: 'pointer' }} onClick={() => handleNetworkSwitch(ChainId.BSC)} />
-          <Poly sx={{ marginRight: '10px', cursor: 'pointer' }} onClick={() => handleNetworkSwitch(ChainId.MATIC)} />
-          <Tlos sx={{ marginRight: '10px', cursor: 'pointer' }} onClick={() => handleNetworkSwitch(ChainId.TLOS)} />
-          <Arbitrum sx={{ cursor: 'pointer' }} onClick={() => handleNetworkSwitch(ChainId.ARBITRUM)} />
+          <Bnb
+            sx={{ marginRight: '10px', cursor: 'pointer' }}
+            onClick={() => window.open('https://v3.apeswap.finance/add-liquidity')}
+          />
+          <Poly
+            sx={{ marginRight: '10px', cursor: 'pointer' }}
+            onClick={() => window.open('https://v3.apeswap.finance/add-liquidity')}
+          />
         </Flex>
         <Link to="/add-liquidity">
           <Flex sx={styles.billImage}>
@@ -54,11 +48,12 @@ const ApeSwapV3 = () => {
           >
             {t('Learn more')}
           </Button>
-          <Link to="/add-liquidity">
-            <Button sx={{ fontSize: ['14px', '14px', '16px'], minWidth: ['140px', '140px'] }}>
-              {t('Add liquidity')}
-            </Button>
-          </Link>
+          <Button
+            sx={{ fontSize: ['14px', '14px', '16px'], minWidth: ['140px', '140px'] }}
+            onClick={() => window.open('https://v3.apeswap.finance/add-liquidity')}
+          >
+            {t('Add liquidity')}
+          </Button>
         </Flex>
       </Flex>
       <Flex sx={{ width: ['0', '100%'], justifyContent: 'center' }}>
